05b5eacad5e415f9182fef2df43bdc7c40b16074
FEDESSS <vila.glebova.1996@gmail.com>
2025-08-02 19:29:08 +0500

    delete element for list

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ type element struct {
 	next *element
 }
 
-func newList(data int) element {
+func NewList(data int) element {
 	return element{data, nil}
 }
 
-func printList(list *element) {
+func PrintList(list *element) {
 	for {
 		fmt.Println(list.data)
 		if list.next != nil {
@@ -24,7 +24,7 @@ func printList(list *element) {
 	}
 }
 
-func addEndList(list *element, data int) {
+func AddEndList(list *element, data int) {
 	for {
 		if list.next != nil {
 			list = list.next
@@ -35,11 +35,11 @@ func addEndList(list *element, data int) {
 	}
 }
 
-func addStartList(list element, data int) element {
+func AddStartList(list element, data int) element {
 	return element{data: data, next: &list}
 }
 
-func addAfter(list *element, index int, data int) {
+func AddAfter(list *element, index int, data int) {
 	counter := 0
 	for {
 		if counter != index {
@@ -60,10 +60,60 @@ func addAfter(list *element, index int, data int) {
 
 }
 
+func DeleteEndList(list *element) {
+	for {
+		if list.next != nil {
+			list = list.next
+			if list.next.next == nil {
+				list.next = nil
+				break
+			}
+		}
+
+	}
+}
+
+func DeleteStartList(list *element) element {
+	if list.next != nil {
+		return *list.next
+	} else {
+		fmt.Println("object not found")
+	}
+	return *list
+}
+
+func DeleteAfterList(list *element, index int) {
+	counter := 0
+	for {
+		if counter != index {
+			if list.next != nil {
+				counter++
+				list = list.next
+			} else {
+				fmt.Println("object not found")
+				break
+			}
+
+		} else {
+			list.next = list.next.next
+			break
+		}
+	}
+}
+
 func main() {
-	list := newList(12)
-	addEndList(&list, 14)
-	list = addStartList(list, 0)
-	addAfter(&list, 0, 69)
-	printList(&list)
+	list := NewList(12)
+	AddEndList(&list, 14)
+	list = AddStartList(list, 0)
+	AddAfter(&list, 0, 69)
+	PrintList(&list)
+	fmt.Println("=====================")
+	DeleteEndList(&list)
+	PrintList(&list)
+	fmt.Println("=====================")
+	list = DeleteStartList(&list)
+	PrintList(&list)
+	fmt.Println("=====================")
+	DeleteAfterList(&list, 0)
+	PrintList(&list)
 }
